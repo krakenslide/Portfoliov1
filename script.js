9b33a0d935ec9b370bbfe257c4a23a0ddccb0569
Ankit Mukhopadhyay <muk.ankit@gmail.com>
2024-01-26 08:58:47 +0530

    Resume button change

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const toggleMenu = () => {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 };
+
+var autoType = new Typed(".autoType", {
+    strings: ["Ankit Mukhopadhyay", "Coding", "Bug Bashing"],
+    typeSpeed: 200,
+    backSpeed: 200,
+    loop: true
+})
