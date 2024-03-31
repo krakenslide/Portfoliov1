8922ccea1c5c98dfa5f4f0249dc40102d77d5504
Ankit Mukhopadhyay <muk.ankit@gmail.com>
2024-03-31 23:51:23 +0530

    Proj changes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const toggleMenu = () => {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 };
 
 var autoType = new Typed(".autoType", {
-    strings: ["Ankit", "Coding", "Bug Bashing"],
-    typeSpeed: 200,
-    backSpeed: 200,
-    loop: true
-})
+  strings: ["Ankit", "Coding", "Bug Bashing"],
+  typeSpeed: 200,
+  backSpeed: 200,
+  loop: true,
+});
